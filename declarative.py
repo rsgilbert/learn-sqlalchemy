939f84b3e22f212c4fe9d29a93048cdc732ec55e
SSG <gssenyonjo@gmail.com>
2023-04-22 20:37:21 +0300

    Defined User and Address using sqlalchemy ORM in a declarative nature in declarative.py

--- declarative.py
+++ declarative.py
@@ -1,9 +1,54 @@
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, ForeignKey, String
+from typing import Optional, List
+
+
+engine = create_engine('sqlite+pysqlite:///:memory:', echo=True)
+
+
 # Define table in a declarative way
 
-from sqlalchemy.orm import DeclarativeBase
 
 class Base(DeclarativeBase):
     pass
 
-print(Base.metadata)
-print(Base.registry)
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(30))
+    fullname: Mapped[Optional[str]]
+    addresses: Mapped[List["Address"]] = relationship(back_populates="user")
+
+    def __repr__(self) -> str:
+        return f"User(id={self.id!r}, name={self.name!r}, fullname={self.fullname!r})"
+
+
+class Address(Base):
+    __tablename__ = "address"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    email_address: Mapped[str]
+    user_id = mapped_column(ForeignKey("user.id"))
+    user: Mapped[User] = relationship(back_populates="addresses")
+
+    def __repr__(self) -> str:
+        return f"Address(id={self.id!r}, email_address={self.email_address!r})"
+
+
+Base.metadata.create_all(engine)
+
+user = User(fullname="Jonathan", name="Peter")
+print(user)
+
+
+
+
+
+
+
+
+
+
+
