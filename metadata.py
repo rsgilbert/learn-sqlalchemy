ac74b6cc9ea7cc4df21cd7c11da8711658efb9f1
SSG <gssenyonjo@gmail.com>
2023-04-22 19:46:54 +0300

    Defined foreign key relationship between address and user_account

--- metadata.py
+++ metadata.py
@@ -1,4 +1,4 @@
-from sqlalchemy import MetaData, Table, Column, Integer, String
+from sqlalchemy import MetaData, Table, Column, Integer, ForeignKey, String
 
 metadata_obj = MetaData()
 
@@ -17,3 +17,11 @@ print(user_table.c.name)
 print(user_table.c.fullname)
 print(user_table.primary_key)
 
+
+address_table = Table(
+    "address",
+    metadata_obj,
+    Column("id", Integer, primary_key=True),
+    Column("user_id", ForeignKey("user_account.id"), nullable=False),
+    Column("email_address", String, nullable=False)
+)
